fed833607c9bdaea5b9f05324db0344d3324a8ee
vkfbmzef <monkeylordjunior@gmail.com>
2020-10-07 18:35:29 -0700

    finishing touches

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,117 @@
-// array of questions for user
-const questions = [
+//global declarations
+// import inquirer
+const inquirer = require('inquirer');
+// import readMe function from generateMarkdown js file - format data/readMe
+const readMe = require('./utils/generateMarkdown.js');
 
+// array containing questions
+const questions = [
+{
+// title
+type: 'input',
+name: 'title',
+message: 'What is the name of this project?',
+validate: nameInput => validateInput(nameInput)
+},
+{
+// description
+type: 'input',
+name: 'description',
+message: 'What is a quick description for said project?',
+validate: nameInput => validateInput(nameInput)
+},
+{
+//installation
+type: 'input',
+name: 'installation',
+message: 'Are there any required installs?',
+validate: nameInput => validateInput(nameInput)
+},
+{
+// usage
+type: 'input',
+name: 'usage',
+message: 'What are the instructions for using this app?',
+validate: nameInput => validateInput(nameInput)
+},
+{
+// contribute
+type: 'input',
+name: 'contributing',
+message: 'How can I contribute?',
+validate: nameInput => validateInput(nameInput)
+},
+{
+// test
+type: 'input',
+name: 'tests',
+message: 'How can I test this project?',
+validate: nameInput => validateInput(nameInput)
+},
+{
+// license
+type: 'checkbox',
+name: 'license',
+message: 'What License does your project have? (Select all that apply)',
+// GNU is a GENERAL PUBLIC LICENSE
+choices: ['Apache', 'GNU', 'ISC', 'Native', 'MIT']      
+},
+{
+// username
+type: 'input',
+name: 'username',
+message: 'What is your Github username?',
+validate: nameInput => validateInput(nameInput)
+},
+{
+// email
+type: 'input',
+name: 'email',
+message: 'What is your email address?',
+validate: nameInput => validateInput(nameInput)
+}
 ];
 
+// function to validate user answer
+const validateInput = userInput => {
+    if (userInput) {
+        return true;
+    } else {
+        console.log("Please try again");
+        return false;
+    }
+};
+
+// enable fs package
+const fs = require('fs');
 // function to write README file
-function writeToFile(fileName, data) {
-}
+const writeToFile = fileContent => {
+    //place file in dist folder
+    fs.writeFile('./dist/README.md', fileContent, err => {
+        if (err) throw err;
+        console.log('README.md complete! See README.md in your dist folder. For more tips/tricks: https://guides.github.com/features/wikis/');
+    });
+};
 
 // function to initialize program
-function init() {
-
+function init(questions) {
+    // use inquirer to list questions
+    return inquirer.prompt(questions);
 }
 
 // function call to initialize program
-init();
+init(questions)
+// pass object containing user input to README function 
+.then(userAnswers => {
+    return readMe(userAnswers);
+})
+// pass string template literal from readMe output
+.then(sameData => {
+    // add to dist folder
+    return writeToFile(sameData);
+})
+.catch(err => {
+    console.log(err);
+});
+
+// finito
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,8 +1,55 @@
-// function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
 
+// function to print license
+const printLicense = selectLicense => {
+  // exit if none
+if (selectLicense.length === 0) {
+    return '';
+  } else {
+    return `
+## Project licensing
+${selectLicense.map(x => {
+  return "\n * " + x;
+})}
+`}
+};
+
+// function to generate markdown for README
+function readMe(data) {
+// destructure and return
+const { title, description, installation, usage, contributing, tests, license, username, email} = data;
+return `
+# ${title}
+![](https://img.shields.io/badge/license-${license}-orange?style=for-the-badge&logo=appveyor)
+## Table of Contents
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [License](#license)
+* [Support](#username)
+## Description 
+${description}
+## Installation
+To install necessary dependencies, run the following command(s):
+${installation}
+## Usage
+${usage}
+## Contributing
+${contributing}
+## Tests
+Testing information:
+${tests}
+## License
+${printLicense(license)}
+## Live URL
+https://${username}.github.io/${title}/
+## Support
+Github:
+https://github.com/${username}/${title}
+Email:
+${email}
 `;
 }
 
-module.exports = generateMarkdown;
+//export function
+module.exports = readMe;
